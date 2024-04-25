acc0f270b459695fb8e806a8265066bc50ca81ad
chetan <singhctn2001@gmail.com>
2024-04-25 13:29:42 +0530

    created states
    creating the states for data such as tasks and using them to add delete and handle data

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,47 @@
-import React from "react";
+"use client";
+import React, { ReactNode, useState } from "react";
 
-const items = [
-  "afsfdv",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-  "Adsfga",
-  "adsfSFA",
-  "ASFDARGV",
-];
+type taskType = {
+  _id: number | string | null;
+  task: string;
+  done: boolean;
+};
 
-export default function page() {
+export default function Page() {
+  const [items, setItems] = useState<Array<taskType>>([]);
+
+  const [task, setTask] = useState<taskType>({
+    _id: null,
+    task: "",
+    done: false,
+  });
+
+  const handleChange = (e: any) => {
+    setTask((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+      _id: items.length + 1,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    items.push(task);
+    setTask({ _id: null, task: "", done: false });
+  };
+
+  const onToggleItem = (id: any) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item._id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  };
+
+  const handleDeleteTask = (id: any) => {
+    setItems((prev) => prev.filter((item) => item._id !== id));
+  };
+
   return (
     <div className="flex flex-col justify-center items-center bg-gradient-to-t from-black via-pink-500 to-purple-400">
       <header className="h-20 flex justify-center items-center">
@@ -37,18 +50,17 @@ export default function page() {
       <h1 className="text-4xl m-10">
         Add the Tasks to the list:<span> </span>
       </h1>
-      <form className="text-2xl flex justify-center items-center w-full ">
-        <select className="bg-transparent border border-white rounded-xl p-3 w-36 mx-4 focus:outline-none">
-          {Array.from({ length: 20 }, (_, i) => i + 1).map((val) => (
-            <option value={val} key={val}>
-              {val}
-            </option>
-          ))}
-        </select>
+      <form
+        className="text-2xl flex justify-center items-center w-full"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           placeholder="Task*"
-          className="bg-transparent border border-white rounded-xl p-3 mx-4 focus:outline-none"
+          className="bg-transparent border border-white rounded-xl p-3 mx-4 focus:outline-none w-80"
+          name="task"
+          onChange={handleChange}
+          value={task.task}
         />
         <button className="bg-transparent border border-white rounded-xl p-3 mx-4 focus:outline-none">
           Add Item
@@ -58,18 +70,53 @@ export default function page() {
       <div className="bg-[#1a1a1a]/20 backdrop-opacity-10 m-10 w-[80%] h-[50vh]">
         <ul className="overflow-auto h-full w-full m-auto grid grid-cols-3 gap-4 justify-items-center">
           {items.map((item) => (
-            <li key={item} className="text-2xl m-3 p-2 justify-items-center">
-              <input type="checkbox" className="h-5 w-5" />
-              <span className="inline-block w-44 px-2">{item} </span>
-              <button className="text-red-600 text-4xl">&times;</button>
+            <li
+              key={item._id}
+              className="text-2xl m-3 p-2 justify-items-center"
+            >
+              <input
+                type="checkbox"
+                checked={item.done}
+                onChange={() => onToggleItem(item._id)}
+                className="h-5 w-5"
+              />
+              <span
+                className={`inline-block w-44 px-2 ${
+                  item.done
+                    ? "line-through decoration-red-700 decoration-4 "
+                    : ""
+                }`}
+              >
+                {item.task}
+              </span>
+              <button
+                className="text-red-600 text-4xl"
+                onClick={() => handleDeleteTask(item._id)}
+              >
+                &times;
+              </button>
             </li>
           ))}
         </ul>
       </div>
 
-      <div>Sorting</div>
+      <div className="m-6">
+        <select className="bg-transparent border border-white rounded-xl p-3 mx-4 focus:outline-none text-2xl w-64">
+          <option value="" className="text-black p-3">
+            Latest first
+          </option>
+          <option value="" className="text-black">
+            Old first
+          </option>
+          <option value="" className="text-black">
+            Alphabatically
+          </option>
+        </select>
+      </div>
 
-      <footer>&copy; {new Date().getFullYear()} Chetan Singh</footer>
+      <footer className="m-6 text-2xl mb-10">
+        &copy; {new Date().getFullYear()} Chetan Singh
+      </footer>
     </div>
   );
 }
